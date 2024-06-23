4b129cb5f1302d1e11f0c70e0dbdb08333aa302c
dumplepo <253725209+dumplepo@users.noreply.github.com>
2024-06-23 16:28:14 +0700

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -7,24 +7,14 @@ import json
 import glob
 import shutil
 
-
 def prepare_dataset(root_dir=None, output_dir=None, train=True, rm_files=True):
-    if train:
-        root = os.path.join(root_dir, 'train')
-    else:
-        root = os.path.join(root_dir, 'valid')
-
+    root = os.path.join(root_dir, 'train' if train else 'valid')
     global_width = 640
     global_height = 640
     if rm_files:
         if os.path.isdir(output_dir):
             shutil.rmtree(output_dir)
         os.makedirs(output_dir)
-
-        # os.makedirs(os.path.join(output_dir, 'images', 'train'))
-        # os.makedirs(os.path.join(output_dir, 'images', 'val'))
-        # os.makedirs(os.path.join(output_dir, 'labels', 'train'))
-        # os.makedirs(os.path.join(output_dir, 'labels', 'val'))
 
         os.makedirs(os.path.join(output_dir, 'train', 'images'))
         os.makedirs(os.path.join(output_dir, 'train', 'labels'))
@@ -43,68 +33,39 @@ def prepare_dataset(root_dir=None, output_dir=None, train=True, rm_files=True):
         images_json = data['images']
         annotations = data['annotations']
 
-    if train:
-        count = 1
-        for image in images_name:
-            image_path = os.path.join(root, image)
-            read_image = cv2.imread(image_path)
-            cv2.imwrite(os.path.join(output_dir, 'train', 'images', '{}.jpg'.format(count)), read_image)
+    count = 1
+    for image in images_name:
+        image_path = os.path.join(root, image)
+        read_image = cv2.imread(image_path)
+        cv2.imwrite(os.path.join(output_dir, 'train' if train else 'valid', 'images', '{}.jpg'.format(count)),
+                    read_image)
+        current_bboxes = []
+        for obj in images_json:
+            if image == obj['file_name']:
+                image_id = obj['id']
+                for annotation in annotations:
+                    if annotation['image_id'] == image_id:
+                        bbox = annotation['bbox']
+                        category_id = annotation['category_id']
+                        current_bboxes.append([bbox, category_id])
 
-            current_bboxes = []
-            for obj in images_json:
-                if image == obj['file_name']:
-                    image_id = obj['id']
-                    for annotation in annotations:
-                        if annotation['image_id'] == image_id:
-                            bbox = annotation['bbox']
-                            category_id = annotation['category_id']
-                            current_bboxes.append([bbox, category_id])
-
-            with open(os.path.join(output_dir, 'train', 'labels', '{}.txt'.format(count)), 'w') as text_file:
-                for box in current_bboxes:
-                    (xmin, ymin, width, height), cat = box
-                    xcent = (xmin + width / 2) / global_width
-                    ycent = (ymin + height / 2) / global_height
-                    width /= global_width
-                    height /= global_height
-                    text_file.write("{} {:.6f} {:.6f} {:.6f} {:.6f}\n".format(cat - 1, xcent, ycent, width, height))
-
-            count += 1
-            print(count)
-    else:
-        count = 1
-        for image in images_name:
-            image_path = os.path.join(root, image)
-            read_image = cv2.imread(image_path)
-            cv2.imwrite(os.path.join(output_dir, 'valid', 'images', '{}.jpg'.format(count)), read_image)
-
-            current_bboxes = []
-            for obj in images_json:
-                if image == obj['file_name']:
-                    image_id = obj['id']
-                    for annotation in annotations:
-                        if annotation['image_id'] == image_id:
-                            bbox = annotation['bbox']
-                            category_id = annotation['category_id']
-                            current_bboxes.append([bbox, category_id])
-
-            with open(os.path.join(output_dir, 'valid', 'labels', '{}.txt'.format(count)), 'w') as text_file:
-                for box in current_bboxes:
-                    (xmin, ymin, width, height), cat = box
-                    xcent = (xmin + width / 2) / global_width
-                    ycent = (ymin + height / 2) / global_height
-                    width /= global_width
-                    height /= global_height
-                    text_file.write("{} {:.6f} {:.6f} {:.6f} {:.6f}\n".format(cat - 1, xcent, ycent, width, height))
-
-            count += 1
-            print(count)
+        filename = os.path.join(output_dir, 'train' if train else 'valid', 'labels', '{}.txt'.format(count))
+        with open(filename, 'w') as text_file:
+            for box in current_bboxes:
+                (xmin, ymin, width, height), cat = box
+                xcent = (xmin + width / 2) / global_width
+                ycent = (ymin + height / 2) / global_height
+                width /= global_width
+                height /= global_height
+                text_file.write("{} {:.6f} {:.6f} {:.6f} {:.6f}\n".format(cat - 1, xcent, ycent, width, height))
+        count += 1
+        print(count)
 
 
 if __name__ == '__main__':
     prepare_dataset(root_dir='dataset_coco/player', output_dir='dataset_yolo/player', train=True, rm_files=True)
     prepare_dataset(root_dir='dataset_coco/player', output_dir='dataset_yolo/player', train=False, rm_files=False)
-    prepare_dataset(root_dir='dataset_coco/court', output_dir='dataset_yolo/court', train=True, rm_files=True)
-    prepare_dataset(root_dir='dataset_coco/court', output_dir='dataset_yolo/court', train=False, rm_files=False)
+    # prepare_dataset(root_dir='dataset_coco/court', output_dir='dataset_yolo/court', train=True, rm_files=True)
+    # prepare_dataset(root_dir='dataset_coco/court', output_dir='dataset_yolo/court', train=False, rm_files=False)
     prepare_dataset(root_dir='dataset_coco/shuttle', output_dir='dataset_yolo/shuttle', train=True, rm_files=True)
     prepare_dataset(root_dir='dataset_coco/shuttle', output_dir='dataset_yolo/shuttle', train=False, rm_files=False)
